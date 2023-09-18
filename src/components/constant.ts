74cfaa0e35c9f6d9b292b2fba42d0e24436c5bc8
Umar <umarnaveed001@gmail.com>
2023-09-18 17:15:48 +0500

    resolve issues

--- src/components/constant.ts
+++ src/components/constant.ts
@@ -1,5 +1,5 @@
 
 
-// export const baseUrl = "https://mmkyneftb2.execute-api.us-east-1.amazonaws.com"
-export const baseUrl = "http://localhost:4000"
+export const baseUrl = "https://mmkyneftb2.execute-api.us-east-1.amazonaws.com"
+// export const baseUrl = "http://localhost:4000"
 export const JWT_TOKEN = "authorization"
